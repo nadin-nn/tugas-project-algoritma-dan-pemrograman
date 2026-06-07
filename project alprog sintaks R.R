849e38652a8d2db01b470198a18483e42f6482f8
nadin-nn <aishanadin36@gmail.com>
2026-06-07 14:23:41 +0700

    Update project alprog sintaks R.R

--- project alprog sintaks R.R
+++ project alprog sintaks R.R
@@ -1,8 +1,8 @@
 #program 2
-K1 <- "saya tak 'kan menyerah."
-K2 <- 'ia berkata, "aku menyayangimu."'
-K3 <- "\"coba jelaskan pengertian 'cross-validation' dalam machine learning!\""
-K4 <- "surat keputusan itu bernomor 62/UN.34/19/2023."
+K1 <- "Saya tak 'kan menyerah."
+K2 <- 'Ia berkata, "Aku menyayangimu."'
+K3 <- "\"Coba jelaskan pengertian 'cross-validation' dalam Machine Learning!\""
+K4 <- "Surat keputusan itu bernomor 62/UN.34/19/2023."
 
 cat(K1, "\n")
 cat(K2, "\n")
