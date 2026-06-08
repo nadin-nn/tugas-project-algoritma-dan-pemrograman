14afe23ce00481f391bd3ddb10344519c54e51b2
nadin-nn <aishanadin36@gmail.com>
2026-06-08 23:17:56 +0700

    Update project alprog sintaks R.R

--- project alprog sintaks R.R
+++ project alprog sintaks R.R
@@ -9,6 +9,7 @@ cat(K2, "\n")
 cat(K3, "\n")
 cat(K4)
 
+
 #program 3
 nilai_koefisien <- function(a, b, c) {
   D <- b^2-4*a*c 
@@ -33,6 +34,37 @@ nilai_koefisien <- function(a, b, c) {
 nilai_koefisien(1, 0, -4)
 nilai_koefisien(1, -4, 4)
 nilai_koefisien(1, 2, 2)
+
+
+#program 5
+hitung_jarak <- function(x1, x2, x3) {
+  jarak_ke_A <- sqrt((x1-2)^2 + (x2-1)^2 + (x3-3)^2)
+  jarak_ke_B <- sqrt((x1-1)^2 + (x2+4)^2 + (x3-6)^2)
+  jarak_ke_C <- sqrt((x1+2)^2 + (x2-3)^2 + (x3+2)^2)
+  return(
+    list(
+      jarak_UA = jarak_ke_A,
+      jarak_UB = jarak_ke_B,
+      jarak_UC = jarak_ke_C
+    )
+  )
+}
+hasil_jarak <- hitung_jarak(2, 5, 6)
+print(hasil_jarak)
+
+cluster <- function(hasil_jarak) {
+  if (hasil_jarak$jarak_UA < hasil_jarak$jarak_UB & hasil_jarak$jarak_UA < hasil_jarak$jarak_UC) {
+    cat("anggota cluster A")
+  } else if (hasil_jarak$jarak_UB < hasil_jarak$jarak_UA & hasil_jarak$jarak_UB < hasil_jarak$jarak_UC) {
+    cat("anggota cluster B")
+  } else {
+    cat("anggota cluster C")
+  }
+}
+cluster(hasil_jarak)
+
+
+
 
 
 
