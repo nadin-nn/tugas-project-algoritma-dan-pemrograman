4b7a488b596c682c007218a445bc48b7bd6de630
nadin-nn <aishanadin36@gmail.com>
2026-06-08 22:02:18 +0700

    Update project alprog sintaks R.R

--- project alprog sintaks R.R
+++ project alprog sintaks R.R
@@ -9,4 +9,41 @@ cat(K2, "\n")
 cat(K3, "\n")
 cat(K4)
 
-tes
+#program 3
+nilai_koefisien <- function(a, b, c) {
+  D <- b^2-4*a*c 
+  if (D > 0) {
+    x1 <- (-b+sqrt(D))/2*a
+    x2 <- (-b-sqrt(D))/2*a
+    cat("akar real:", "\n")
+    cat("x1 =", x1, "\n")
+    cat("x2 =", x2, "\n")
+  } else if (D == 0) {
+    x <- -b/(2*a)
+    cat("akar kembar:", "\n")
+    cat("x =", x, "\n")
+  } else {
+    x1 <- (-b+sqrt(as.complex(D)))/2*a
+    x2 <- (-b-sqrt(as.complex(D)))/2*a
+    cat("akar imajiner:", "\n")
+    cat("x1 =", x1, "\n")
+    cat("x2 =", x2)
+  }
+}
+nilai_koefisien(1, 0, -4)
+nilai_koefisien(1, -4, 4)
+nilai_koefisien(1, 2, 2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
