bd19288ce66efde5530a4cd282bbdc8f3c549766
galang-maker <galang.utomo@mhs.unsoed.ac.id>
2026-06-09 11:12:43 +0700

    Update project alprog sintaks R.R

--- project alprog sintaks R.R
+++ project alprog sintaks R.R
@@ -1,6 +1,6 @@
 #program 1
 
-teks <- ""
+teks <- "Media sosial atau disebut juga dengan jejaring sosial, seperti Facebook, Twitter, Instagram, dan masih banyak lagi ternyata tidak hanya digunakan sebagai tempat berkumpul atau berbagi di dunia maya. Namun, media sosial kini juga bisa dimanfaatkan sebagai media untuk mengembangkan sebuah bisnis. Saat ini telah banyak para pengusaha yang beralih ke media sosial dalam memasarkan produk mereka baik barang ataupun jasa. Beralihnya para pelaku bisnis ke media ini dikarenakan jejaring sosial memiliki manfaat yang sangat banyak bagi usaha bisnis. Berikut ini adalah alasan mengapa jejaring sosial bisa menjadi alat promosi yang paling efektif."
 
 kata <- unlist(strsplit(teks, " "))
 jumlah_kata <- length(kata)
