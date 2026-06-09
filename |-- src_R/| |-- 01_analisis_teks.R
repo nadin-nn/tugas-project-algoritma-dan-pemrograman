c0851097648018dfb2ffe54eaf82371e21c35dbd
nadin-nn <aishanadin36@gmail.com>
2026-06-10 00:09:15 +0700

    Update | |-- 01_analisis_teks.R

--- |-- src_R/| |-- 01_analisis_teks.R
+++ |-- src_R/| |-- 01_analisis_teks.R
@@ -1,1 +1,12 @@
+teks <- "Media sosial atau disebut juga dengan jejaring sosial, seperti Facebook, Twitter, Instagram, dan masih banyak lagi ternyata tidak hanya digunakan sebagai tempat berkumpul atau berbagi di dunia maya. Namun, media sosial kini juga bisa dimanfaatkan sebagai media untuk mengembangkan sebuah bisnis. Saat ini telah banyak para pengusaha yang beralih ke media sosial dalam memasarkan produk mereka baik barang ataupun jasa. Beralihnya para pelaku bisnis ke media ini dikarenakan jejaring sosial memiliki manfaat yang sangat banyak bagi usaha bisnis. Berikut ini adalah alasan mengapa jejaring sosial bisa menjadi alat promosi yang paling efektif."
 
+kata <- unlist(strsplit(teks, " "))
+jumlah_kata <- length(kata)
+
+kalimat <- unlist(strsplit(teks, "\\."))
+jumlah_kalimat <- sum(trimws(kalimat) != "")
+
+cat("Teks:\n")
+cat(teks, "\n\n")
+cat("Jumlah kata =", jumlah_kata, "\n")
+cat("Jumlah kalimat =", jumlah_kalimat, "\n")
