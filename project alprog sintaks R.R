e8405d8d433a710dd913192114ea0be4935b52c3
syahsiyakamila86-design <syahsiyakamila86@gmail.com>
2026-06-09 11:50:08 +0700

    Update project alprog sintaks R.R

--- project alprog sintaks R.R
+++ project alprog sintaks R.R
@@ -67,6 +67,46 @@ main <- function() {
 
 main()
 
+
+# program 4
+nip <- "2000042319031010"
+
+tahun <- substr(nip, 1, 4)
+bulan <- substr(nip, 5, 6)
+tanggal <- substr(nip, 7, 8)
+
+if (bulan == "01") {
+  nama_bulan <- "Januari"
+} else if (bulan == "02") {
+  nama_bulan <- "Februari"
+} else if (bulan == "03") {
+  nama_bulan <- "Maret"
+} else if (bulan == "04") {
+  nama_bulan <- "April"
+} else if (bulan == "05") {
+  nama_bulan <- "Mei"
+} else if (bulan == "06") {
+  nama_bulan <- "Juni"
+} else if (bulan == "07") {
+  nama_bulan <- "Juli"
+} else if (bulan == "08") {
+  nama_bulan <- "Agustus"
+} else if (bulan == "09") {
+  nama_bulan <- "September"
+} else if (bulan == "10") {
+  nama_bulan <- "Oktober"
+} else if (bulan == "11") {
+  nama_bulan <- "November"
+} else if (bulan == "12") {
+  nama_bulan <- "Desember"
+} else {
+  nama_bulan <- "Tidak Valid"
+}
+
+cat("Tanggal Lahir ASN:\n")
+cat(tanggal, nama_bulan, tahun)
+
+
 #program 5
 hitung_jarak <- function(x1, x2, x3) {
   jarak_ke_A <- sqrt((x1-2)^2 + (x2-1)^2 + (x3-3)^2)
