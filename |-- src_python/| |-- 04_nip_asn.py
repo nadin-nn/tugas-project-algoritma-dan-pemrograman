2bcded714bf075f7d30816f13828c29aef56e243
syahsiyakamila86-design <syahsiyakamila86@gmail.com>
2026-06-12 19:11:54 +0700

    Update | |-- 04_nip_asn.py

--- |-- src_python/| |-- 04_nip_asn.py
+++ |-- src_python/| |-- 04_nip_asn.py
@@ -1,36 +1,45 @@
 nip = input("Masukkan NIP ASN: ")
 
-tahun = nip[0:4]
-bulan = nip[4:6]
-tanggal = nip[6:8]
+tahun = int(nip[0:4])
+bulan = int(nip[4:6])
+tanggal = int(nip[6:8])
 
-if bulan == "01":
-    nama_bulan = "Januari"
-elif bulan == "02":
-    nama_bulan = "Februari"
-elif bulan == "03":
-    nama_bulan = "Maret"
-elif bulan == "04":
-    nama_bulan = "April"
-elif bulan == "05":
-    nama_bulan = "Mei"
-elif bulan == "06":
-    nama_bulan = "Juni"
-elif bulan == "07":
-    nama_bulan = "Juli"
-elif bulan == "08":
-    nama_bulan = "Agustus"
-elif bulan == "09":
-    nama_bulan = "September"
-elif bulan == "10":
-    nama_bulan = "Oktober"
-elif bulan == "11":
-    nama_bulan = "November"
-elif bulan == "12":
-    nama_bulan = "Desember"
+if tahun < 1900 or tahun > 2025:
+    print("Tanggal Lahir ASN : Tidak Valid (Tahun)")
+
+elif bulan < 1 or bulan > 12:
+    print("Tanggal Lahir ASN : Tidak Valid (Bulan)")
+
+elif tanggal < 1 or tanggal > 31:
+    print("Tanggal Lahir ASN : Tidak Valid (Tanggal)")
+
 else:
-    nama_bulan = "Tidak Valid"
+    if bulan == 1:
+        nama_bulan = "Januari"
+    elif bulan == 2:
+        nama_bulan = "Februari"
+    elif bulan == 3:
+        nama_bulan = "Maret"
+    elif bulan == 4:
+        nama_bulan = "April"
+    elif bulan == 5:
+        nama_bulan = "Mei"
+    elif bulan == 6:
+        nama_bulan = "Juni"
+    elif bulan == 7:
+        nama_bulan = "Juli"
+    elif bulan == 8:
+        nama_bulan = "Agustus"
+    elif bulan == 9:
+        nama_bulan = "September"
+    elif bulan == 10:
+        nama_bulan = "Oktober"
+    elif bulan == 11:
+        nama_bulan = "November"
+    else:
+        nama_bulan = "Desember"
 
-print("Tanggal Lahir ASN:")
-print(tanggal, nama_bulan, tahun)
-     
+    print("Tanggal Lahir ASN :", tanggal, nama_bulan, tahun)
+
+
+# kon
